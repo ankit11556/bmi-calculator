f7b6ebf2940df875777681484e31d6b525d9b0a9
ankit <saharan442090@.com>
2025-01-16 21:02:06 +0530

    Removed unnecessary comments from code

--- app.js
+++ app.js
@@ -1,32 +1,5 @@
-//let height = document.getElementById('height').value;
-//let Weight = document.getElementById('Weight');
-// let btn = document.getElementById('btn');
-//let result = document.getElementById('result');
-
-// btn.addEventListener('click',()=>{
-//      let height = document.getElementById('height').value;
-//      let Weight = document.getElementById('weight').value;
-//      let result = document.getElementById('result')
-   
-//     if(height === "" || height < 0 || isNaN(height) ){
-//         result.innerHTML = `Please enter valid Height  ${height}`
-//     }
-
-//    else if(Weight === "" ||Weight  < 0 || isNaN(Weight) ){
-//         result.innerHTML = `Please enter valid Weight  ${Weight}`
-//     }
-
-//     else{
-//         const bmi = (Weight / ((height * height) / 10000)).toFixed(2)
-
-//         result.innerHTML = `<span>${bmi}</span>`
-//     }
-// })
-
-
 
 let form = document.querySelector('form');
-// let btn = document.getElementById('btn');
  let bminame = document.getElementById('bminame')
 form.addEventListener('submit',(e)=>{
      e.preventDefault();
